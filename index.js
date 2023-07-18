aee22c2494326e42a19db57b8e4a868e1b1df0e5
Sourav Menon <45852906+souravmenon1999@users.noreply.github.com>
2023-07-18 08:11:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import routes from "./routes.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected!"));
+
+app.use("/", routes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
